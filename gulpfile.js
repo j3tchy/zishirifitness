7a668720e2c1ae563d88b0997af0ffa0fbaaa85a
Jermaine Gyekye <JGyekye@brightsource.co.uk>
2017-09-07 22:44:29 +0000

    header

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('sass', function () {
+    return gulp.src('assets/sass/**/master.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('assets/sass/**/*.scss', ['sass']);
+})
+
+gulp.task('default', ['sass','watch']);
